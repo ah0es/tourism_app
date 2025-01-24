9c6ffb0b90549e3342a1502725b7654eb4c1d7c5
Huda-Mawood <huda.mawod881@compit.aun.edu.eg>
2025-01-24 04:44:50 +0200

    add page hotel and restaurant and plan and

---
+++ lib/features/home/hotelse/presentation/hotels_view_body.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:tourism_app/core/component/custom_card_image_hotels.dart';
+import 'package:tourism_app/core/utils/app_images.dart';
+
+class HotelsViewBody extends StatelessWidget {
+  const HotelsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return CustomCardImageHotels(
+                  country: 'Egypt',
+                  governorate: 'Luxor',
+                  iconData: Icons.location_pin,
+                  image: AppImages.Rectangle2Hotels,
+                  name: 'Four Seasons',
+                  price: 20,
+                  rate: 3,
+                );
+              }),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/home/tourguide/presentation/tourguide_view_body.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:tourism_app/core/component/custom_card_image_tourguide.dart';
+import 'package:tourism_app/core/utils/app_images.dart';
+
+class TourguideViewBody extends StatefulWidget {
+  const TourguideViewBody({super.key});
+
+  @override
+  State<TourguideViewBody> createState() => _TourguideBodyState();
+}
+
+class _TourguideBodyState extends State<TourguideViewBody> {
+  final List<Map<String, dynamic>> tourGuide = [
+    {
+      "image": AppImages.groupTourguide,
+      "language1": 'English',
+      "language2": 'Japanese',
+      "name": 'Ahmed fathy',
+      "rate": 1,
+      "price": 500.0,
+    },
+    {
+      "image": AppImages.groupTourguide,
+      "language1": 'English',
+      "language2": 'Japanese',
+      "name": 'Ahmed fathy',
+      "rate": 1,
+      "price": 500.0,
+    },
+    {
+      "image": AppImages.groupTourguide,
+      "language1": 'English',
+      "language2": 'Japanese',
+      "name": 'Ahmed fathy',
+      "rate": 1,
+      "price": 500.0,
+    },
+    {
+      "image": AppImages.groupTourguide,
+      "language1": 'English',
+      "language2": 'Japanese',
+      "name": 'Ahmed fathy',
+      "rate": 1,
+      "price": 500.0,
+    },
+    {
+      "image": AppImages.groupTourguide,
+      "language1": 'English',
+      "language2": 'Japanese',
+      "name": 'Ahmed fathy',
+      "rate": 1,
+      "price": 500.0,
+    },
+    {
+      "image": AppImages.groupTourguide,
+      "language1": 'English',
+      "language2": 'Japanese',
+      "name": 'Ahmed fathy',
+      "rate": 1,
+      "price": 500.0,
+    },
+    {
+      "image": AppImages.groupTourguide,
+      "language1": 'English',
+      "language2": 'Japanese',
+      "name": 'Ahmed fathy',
+      "rate": 1,
+      "price": 500.0,
+    }
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: tourGuide.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.8,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+            ),
+            itemBuilder: (context, index) {
+              final tourguide = tourGuide[index];
+              return CustomCardImageTourguide(
+                  name: tourguide["name"],
+                  rate: tourguide["rate"],
+                  image: tourguide["image"],
+                  language1: tourguide["language1"],
+                  language2: tourguide["language2"],
+                  price: tourguide["price"]);
+            }),
+      ),
+    );
+  }
+}
